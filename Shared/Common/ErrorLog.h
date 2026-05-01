a065443607600b10275d05638c9e6590072f95b7
cocoz93 <wndnwls7@gmail.com>
2026-05-02 01:17:42 +0900

    나올수있는 GetLastError 추가

--- Shared/Common/ErrorLog.h
+++ Shared/Common/ErrorLog.h
@@ -10,6 +10,10 @@ inline bool ShouldIgnoreWsaError(int errorCode)
 {
     switch (errorCode)
     {
+    // Per-IO방식 사용 시 나올수있는 에러
+    case 10004: // WSAEINTR : 사용자쪽에서 진행중인 I/O를 제거 (closesocket / CancelIO)
+    case 10038: // WSAENOTSOCK: 유효하지 않은 소켓 핸들 (이미 close되었거나 잘못된 값)
+
     case 10053: // WSAECONNABORTED: 소프트웨어적으로 연결 중단 (타임아웃, 프로토콜 오류 등)
     case 10054: // WSAECONNRESET: 상대방이 연결을 강제로 끊음 (RST 수신)
     case 10064: // WSAEHOSTDOWN: 상대 호스트가 다운됨
